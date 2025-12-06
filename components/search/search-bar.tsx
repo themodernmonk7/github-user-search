94c7445fe03df4f6c381eb510fbdf32adc2ff918
themodernmonk7 <sauravavishek@gmail.com>
2025-12-06 19:36:15 +0530

    add debounce

--- components/search/search-bar.tsx
+++ components/search/search-bar.tsx
@@ -1,25 +1,33 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Search } from "lucide-react"
 
 interface SearchBarProps {
   onSearch: (query: string) => void
 }
+const DEBOUNCE_DELAY_VALUE = 500
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [query, setQuery] = useState("")
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    onSearch(query.trim())
-  }
+  // const debouncedValue = useDebounce(query, DEBOUNCE_DELAY_VALUE)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onSearch(query.trim())
+    }, DEBOUNCE_DELAY_VALUE)
+
+    return () => clearTimeout(timer)
+  }, [query])
 
   return (
     <div className="max-w-2xl mx-auto mt-8">
-      <form onSubmit={handleSubmit} className="relative">
+      {/* <form onSubmit={handleSubmit} className="relative"> */}
+      <form className="relative">
         <input
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
+          // onChange={(e) => handleInputChange(e)}
           placeholder="Search GitHub username..."
           className="w-full pl-12 pr-4 py-3 text-gray-700 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all"
         />
---
+++ hooks/use-debounce.tsx
@@ -0,0 +1,20 @@
+// export const useDebounce = (query, delay) => {
+//   let timer
+
+//   return function () {
+//     timer = setTimeout(() => {
+
+//     }, delay)
+//   }
+// }
+
+// // const debounce = (fn, delay) => {
+// //   let timer
+
+// //   if (timer) {
+// //     clearTimeout(timer)
+// //   }
+// //   return function () {
+// //     const timer = setTimeout(() => {}, delay)
+// //   }
+// // }
